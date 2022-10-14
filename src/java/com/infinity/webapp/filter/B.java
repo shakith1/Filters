7b8a049209e71fa6bb5a1403e335f13e2d7a57dd
Shakith <nimsara.shakith@gmail.com>
2022-10-14 10:02:00 +0530

    Filters

---
+++ src/java/com/infinity/webapp/filter/B.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.infinity.webapp.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *
+ * @author Shakith
+ */
+@WebFilter(filterName = "B", urlPatterns = "/Login")
+public class B implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("B do Filter");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

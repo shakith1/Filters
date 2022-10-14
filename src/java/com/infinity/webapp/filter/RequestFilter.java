a2f577069530b1d849deb2a8d50d32765a2ed6f0
Shakith <nimsara.shakith@gmail.com>
2022-10-14 09:35:08 +0530

    Filter

--- src/java/com/infinity/webapp/filter/RequestFilter.java
+++ src/java/com/infinity/webapp/filter/RequestFilter.java
@@ -11,11 +11,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
 /**
  *
  * @author Shakith
  */
+@WebFilter(filterName = "RequestFiter",urlPatterns = {"/Login"})
 public class RequestFilter implements Filter{
 
     @Override
--- src/java/com/infinity/webapp/servlet/Login.java
+++ src/java/com/infinity/webapp/servlet/Login.java
@@ -20,8 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Login....");
     }
     
 }

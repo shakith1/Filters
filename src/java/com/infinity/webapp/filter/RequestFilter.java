b1c5b524aaeb4a168033c474ad2214ab4188a20b
Shakith <nimsara.shakith@gmail.com>
2022-10-14 09:29:47 +0530

    Filter

--- src/java/com/infinity/webapp/filter/RequestFilter.java
+++ src/java/com/infinity/webapp/filter/RequestFilter.java
@@ -4,10 +4,33 @@
  */
 package com.infinity.webapp.filter;
 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 /**
  *
  * @author Shakith
  */
-public class RequestFilter {
+public class RequestFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("RequestFilter init...");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+       
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("RequestFilter destroy...");
+    }
     
 }
